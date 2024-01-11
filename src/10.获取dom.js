641b8bb3d65dce6d65a5893647f657cfdbd7acfe
Lv Jingxin <lv510987@163.com>
2024-01-11 16:49:17 +0800

    父子通信

---
+++ src/10.获取dom.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: Lv Jingxin lv510987@163.com
+ * @Date: 2024-01-02 18:01:41
+ * @LastEditors: Lv Jingxin lv510987@163.com
+ * @LastEditTime: 2024-01-11 16:41:43
+ * @FilePath: /my-app/src/App.js
+ * @Description: react 中获取dom
+ */
+import { useRef } from "react";
+// react 中获取dom
+// useRef 生成ref对象绑定dom身上
+// dom可用时，ref.current获取dom
+// dom渲染完成之后才可用
+function App() {
+  const inputVal = useRef(null)
+  const showDom = ()=>{
+    console.log('=========>',inputVal.current);
+    console.dir(inputVal.current.value);
+    
+  }
+  return (
+    <div className="App">
+      <input type="text" ref={inputVal}></input>
+      <button onClick={showDom}>点我</button>
+     </div>
+  );
+}
+
+export default App;
