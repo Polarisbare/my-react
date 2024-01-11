9b609cfdf69dafc3dad58b428a637ace87ae09f6
Lv Jingxin <lv510987@163.com>
2024-01-11 16:59:24 +0800

    children

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-01-02 18:01:41
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-01-11 16:47:58
+ * @LastEditTime: 2024-01-11 16:56:49
  * @FilePath: /my-app/src/App.js
- * @Description: 父子通信
+ * @Description: children 特殊
  */
 function Son(props){
+  console.log(props);
   return( 
-  <div>son{props.name}</div>
+  <div>son{props.children}</div>
   )
 }
 function App() {
-  const name = 'app'
+  
   return (
     <div className="App">
-     <Son name={name}></Son>
+     <Son >
+      <span> children</span>
+     </Son>
      </div>
   );
 }
