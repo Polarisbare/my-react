b396691b87e352ff93e408dee226e3cbe76eea74
Lv Jingxin <lv510987@163.com>
2024-01-19 14:36:30 +0800

    feat: 测试提交

--- src/12.children.js
+++ src/12.children.js
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-01-02 18:01:41
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-01-11 16:56:49
+ * @LastEditTime: 2024-01-19 14:36:16
  * @FilePath: /my-app/src/App.js
  * @Description: children 特殊
  */
@@ -13,7 +13,7 @@ function Son(props){
   )
 }
 function App() {
-  
+  // 测试
   return (
     <div className="App">
      <Son >
