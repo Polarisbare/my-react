14c47490e96a6cf5894fd24bf32c87cce47add51
Lv Jingxin <lv510987@163.com>
2024-01-19 15:45:20 +0800

    feat: 子传父

--- src/13.子传父参数.js
+++ src/13.子传父参数.js
@@ -4,9 +4,11 @@ import { useState } from "react";
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-01-02 18:01:41
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-01-19 15:37:35
+ * @LastEditTime: 2024-01-19 15:44:54
  * @FilePath: /my-app/src/App.js
  * @Description: 子传父
+ * 
+ * 核心概念：  子组件中调用父组件中的函数并传递实参数
  */
 function Son({onGetMsg}){
   const sonValue = '我是子组件中的数据'
