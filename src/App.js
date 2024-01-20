f5ed068ff70dfff63da4c108daa21ebd895a4012
Lv Jingxin <lv510987@163.com>
2024-01-20 14:39:21 +0800

    feat: useEffect 的使用

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
-import { createContext, useContext } from "react";
-
+import { useEffect, useState } from "react";
 /*
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-01-02 18:01:41
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-01-20 14:07:26
+ * @LastEditTime: 2024-01-20 14:24:39
  * @FilePath: /my-app/src/App.js
- *  跨层传递值
- * App --- A ---- B
  */
-
-// 1. creatContext 方法创建一个上下文对象
-const MsgContext = createContext()
-// 2. 在顶层组件 通过 Provider 组件提供数据
-// 3. 在底层组件 通过 useContext 钩子函数使用数据
-
-function A(){
-  return(
-    <div>
-      this is A
-      <B />
-    </div>
-  )
-}
-function B(){
- const msg = useContext(MsgContext)
-  return(
-    <div>
-      this is B{msg}
-    </div>
-  )
-}
+const URL = 'http://geek.itheima.net/v1_0/channels'
 function App() {
-  const msg = 'this is App msg'
+  const [list , setList] = useState([])
+useEffect( ()=>{
+  // 额外操作   类似生命周期
+  async function getList(){
+  const res = await fetch(URL)
+  const jsonRes = await res.json()
+  setList(jsonRes.data.channels)
+  }
+  getList()
+},[])
   return (
     <div className="App">
       this is App
-      <MsgContext.Provider value={msg}>
-      <A />
-      </MsgContext.Provider>
-
+     <ul>
+      {list.map(item=> <li key={item.id}>{item.name}</li>)}
+     </ul>
      </div>
   );
 }
